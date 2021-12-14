7fbb179190c5ac7ce250458f5ece504e7f12806d
shenhangke <shenhangke@qq.com>
2021-12-14 14:34:48 +0800

    add all update

---
+++ build/tests/CMakeFiles/test_faiss_graph.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_faiss_graph.dir/test_faiss_graph.cpp.o"
+  "test_faiss_graph"
+  "test_faiss_graph.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_faiss_graph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/tests/CMakeFiles/test_nndescent_refine.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/project/knn/tests/test_nndescent_refine.cpp" "/root/project/knn/build/tests/CMakeFiles/test_nndescent_refine.dir/test_nndescent_refine.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "INFO"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../extern_libraries"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/root/project/knn/build/src/CMakeFiles/efanna2e.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
